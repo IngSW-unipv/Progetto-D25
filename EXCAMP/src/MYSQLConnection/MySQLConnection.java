039db932e16959e17fc24af0c230101d3c32cd36
saidaelmo <saida.elmohriri01@universitadipavia.it>
2025-02-08 17:50:13 +0100

    Create MySQLConnection.java

---
+++ EXCAMP/src/MYSQLConnection/MySQLConnection.java
@@ -0,0 +1,24 @@
+package MYSQLConnection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConnection {
+	public static void main(String[] args) {
+        // MySQL database URL
+        String url = "jdbc:mysql://127.0.0.1:3306/testdb";  // Replace with your DB name and MySQL server address
+        String user = "root";  // Replace with your MySQL username
+        String password = "Saida@003";  // Replace with your MySQL password
+
+        try {
+            // Establishing the connection
+            Connection connection = DriverManager.getConnection(url, user, password);
+            if (connection != null) {
+                System.out.println("Connected to the database successfully!");
+            }
+        } catch (SQLException e) {
+            System.out.println("Connection failed!");
+            e.printStackTrace();
+        }
+    }
+}
